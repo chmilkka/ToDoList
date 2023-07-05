270d860692f285d0716b16004eacd9bf0ed4160a
chmilkka <mishachmil228335@gmail.com>
2023-07-05 13:38:49 +0300

    add middleware and log filter

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -31,6 +31,19 @@ namespace ToDoList
 
             app.UseAuthorization();
 
+            app.Use(async (context, next) =>
+            {
+                try
+                {
+                    Console.WriteLine("Something logic BEFORE action");
+                    await next();
+                    Console.WriteLine("Something logic AFTER action");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }           
+            });
 
             app.MapControllers();
 
